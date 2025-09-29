77549c5a224959dbd19a174ae6f7efa2265402d4
minhkeke <teemo2105@gmail.com>
2025-09-29 10:02:41 +0700

    add script to create traffic

--- traffic.sh
+++ traffic.sh
@@ -8,11 +8,11 @@ sudo ip route add 10.45.0.0/16 via 10.53.1.2
 # Routes cho UE
 sudo ip netns exec ue1 ip route replace default via 10.45.1.1 dev tun_srsue
 sudo ip netns exec ue2 ip route replace default via 10.45.2.1 dev tun_srsue
-# sudo ip netns exec ue3 ip route replace default via 10.45.3.1 dev tun_srsue
+sudo ip netns exec ue3 ip route replace default via 10.45.3.1 dev tun_srsue
 
 # iperf song song
 sudo ip netns exec ue1 iperf -c 10.45.1.1 -u -b 10M -i 1 -t 100 > ue1.log 2>&1 &
 sudo ip netns exec ue2 iperf -c 10.45.2.1 -u -b 10M -i 1 -t 100 > ue2.log 2>&1 &
-# sudo ip netns exec ue3 iperf -c 10.45.3.1 -u -b 10M -i 1 -t 100 > ue3.log 2>&1 &
+sudo ip netns exec ue3 iperf -c 10.45.3.1 -u -b 10M -i 1 -t 100 > ue3.log 2>&1 &
 
 wait
